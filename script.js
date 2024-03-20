4f7188295fb26000ac1cb99c928b12b68388a347
umazhar <u.mazhar@icloud.com>
2024-03-19 18:26:46 -0700

    added crowreport starter code

---
+++ script.js
@@ -0,0 +1,14 @@
+// Initialize Swiper 
+var swiper = new Swiper('.swiper-container', {
+    spaceBetween: 30,
+    centeredSlides: true,
+    autoplay: {
+      delay: 2500,
+      disableOnInteraction: false,
+    },
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+  });
+  
