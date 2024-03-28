8feca1c50ba2c3cb72294f7f31e40265b78eeeb9
Yimage Addus <59943772+yimage-web@users.noreply.github.com>
2024-03-28 13:36:30 -0600

    update

--- wc-crowreport/src/components/card.js
+++ wc-crowreport/src/components/card.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const Card = ({ title, content }) => (
-    <a href="#" className="block p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
+    <a className="block p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
         <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{title}</h5>
         {content.map((item , index) => (
             <div key={index} className="mb-2 pb-2"> {/* Add a div here */}
