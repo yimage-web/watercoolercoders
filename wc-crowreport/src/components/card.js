31fc428f64e9b3eaebc0ed08775036a9549193f8
Yimage Addus <59943772+yimage-web@users.noreply.github.com>
2024-03-28 13:47:49 -0600

    Update card.js

--- wc-crowreport/src/components/card.js
+++ wc-crowreport/src/components/card.js
@@ -1,14 +1,14 @@
 import React from 'react';
 
 const Card = ({ title, content }) => (
-    <a className="block p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
+    <p className="block p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
         <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{title}</h5>
         {content.map((item , index) => (
             <div key={index} className="mb-2 pb-2"> {/* Add a div here */}
                 <a href={item.link} className="font-normal text-gray-700 hover:text-gray-400">{item.title}</a>
             </div>
         ))}
-    </a>
+    </p>
 );
 
 const CardsContainer = ({ title, content }) => ( // Destructure the props correctly here
